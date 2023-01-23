fe08ee31f052cf3c2ee64f5de89784a2f8a120e5
Johan Saldes <johan@saldestechnology.com>
2023-01-23 19:18:55 +0100

    implemented counter

--- src/app.rs
+++ src/app.rs
@@ -2,11 +2,18 @@ use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            <button {onclick}>{"+1"}</button>
+            <p>{*counter}</p>
         </main>
     }
 }
